61511b3175c23be617dd5d6b6a5c19015300e5d7
Michiel De Koninck <michieldekoninck2@gmail.com>
2024-09-17 10:17:35 +0200

    first push

---
+++ transcripts_rag/local_llama.py
@@ -0,0 +1,27 @@
+from llama_index.llms.llama_cpp import LlamaCPP
+from llama_index.llms.llama_cpp.llama_utils import (
+    messages_to_prompt,
+    completion_to_prompt,
+)
+
+def load_model(model_url:'str' = 'Meta-Llama-3-8B-Instruct.Q4_0.gguf'):
+    llm = LlamaCPP(
+    # You can pass in the URL to a GGML model to download it automatically
+    model_url=model_url,
+    # optionally, you can set the path to a pre-downloaded model instead of model_url
+    model_path=None,
+    temperature=0.1,
+    max_new_tokens=256,
+    # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
+    context_window=7000,
+    # kwargs to pass to __call__()
+    generate_kwargs={},
+    # kwargs to pass to __init__()
+    # set to at least 1 to use GPU
+    model_kwargs={"n_gpu_layers": -1},
+    # transform inputs into Llama2 format
+    messages_to_prompt=messages_to_prompt,
+    completion_to_prompt=completion_to_prompt,
+    verbose=True,
+    )
+    return llm
---
+++ utils.py
@@ -0,0 +1,11 @@
+import time
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()  # Record the start time
+        result = func(*args, **kwargs)  # Call the actual function
+        end_time = time.time()  # Record the end time
+        elapsed_time = end_time - start_time  # Calculate the elapsed time
+        print(f"Function '{func.__name__}' took {elapsed_time:.6f} seconds to execute.")
+        return result
+    return wrapper
